50b434c40226a7fd94e9c31e2822758932a817df
Chetan Helode <Chetan Helode@DESKTOP-4G5EO2K>
2022-12-30 13:04:04 +0530

    [Chetan]: Java new programs

---
+++ src/Array/Q.java
@@ -0,0 +1,48 @@
+package Array;
+
+public class Q {
+	
+	public static void main(String[] args) {
+		
+	    String brand;       //japanese bike
+	    int cc;
+	    int model;
+	    String manufacture;
+	    
+	    brand = "Suzuki";
+	    cc = 155;
+	    model = 2016;
+	    manufacture = "Japan";
+	    
+	    System.out.println("Brand Name = "+brand);
+	    System.out.println("Bike CC = "+cc);
+	    System.out.println("Bike model year = "+model);
+	    System.out.println("Bike manufacture = "+manufacture);
+	    
+	    indian();
+	}
+	
+	
+	public static void indian()
+	{
+		String brand;      //Indian bike
+		int cc;
+		int model;
+		String manufacture;
+		
+		brand = "Hero";
+		cc = 200;
+		model = 2019;
+		manufacture = "India";
+			
+		System.out.println("\nBrand Name = "+brand);
+		System.out.println("Bike CC = "+cc);
+		System.out.println("Bike model year = "+model);
+		System.out.println("Bike manufacture = "+manufacture);
+		
+	}
+	
+	
+	
+}
+
